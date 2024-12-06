8fa997a35bd1f37b43e05a681232e50322c8a7db
Eric-Formiga <eric.13formiga@gmail.com>
2024-12-05 21:16:54 -0300

    feat: add rendering html

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 
 const port = 3000
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
+
 
 app.get('/', (req, res) => {
   res.send("Olá mundo!")
+
+})
+
+app.get('/html', (req, res) => {
+  res.sendFile(`${basePath}/index.html`)
 
 })
 
